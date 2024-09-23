a798c24bea156b4907177b00ab93eea6c11bd91e
Gafesco <gescobarc3@miumg.edu.gt>
2024-09-23 09:12:31 -0600

    nuevo cambio

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
 import express from 'express'
+import { config } from 'dotenv'
+import pg from 'pg'
+
+config()  // Cargar las variables de entorno
+
 const app = express()
 
-app.get('/', (req, res)=> {
-    res.send('Hola Mundo')
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 })
-app.listen(3000)
-console.log('server on port', 3000)
+
+app.get('/', (req, res) => {
+  res.send('Hola Mundo')
+})
+
+app.get('/ping', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()')
+    return res.json(result.rows[0])
+  } catch (err) {
+    console.error(err)
+    res.status(500).json({ error: 'Database query failed' })
+  }
+})
+
+app.listen(3000, () => {
+  console.log('server on port', 3000)
+})
